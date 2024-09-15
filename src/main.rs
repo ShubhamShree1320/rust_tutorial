d7a3a4811ac7d7cb92d978dfaf3fe0c880af609f
ShubhamShree1320 <shubham.ghostriderdown@gmail.com>
2024-09-15 10:55:57 +0530

    my first rust tutorial
    This is my first comprehensive study on rust

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+mod string_function;
+fn main() {
+    string_function::string_function();
+    let num = 20;
+    println!("{}", is_even(&num));
+    println!("{}", num);
+    println!(
+        "fibonacci number for target = {} is = {}",
+        num,
+        fibonacci(num)
+    )
+}
+fn fibonacci(target: i8) -> i32 {
+    let mut first = 0;
+    let mut second = 1;
+    if target == 0 {
+        return first;
+    }
+    if target == 1 {
+        return 1;
+    }
+    for __ in 1..target - 2 {
+        let tmp = second;
+        second = second + first;
+        first = tmp;
+    }
+    return second;
+}
+fn is_even(number: &i8) -> i8 {
+    if number % 2 == 0 {
+        return number / 2;
+    }
+    return -1;
+}
---
+++ src/string_function.rs
@@ -0,0 +1,5 @@
+pub fn string_function() {
+    let mut s = String::from("hello");
+    s.push_str(", world!");
+    println!("{}", s);
+}
